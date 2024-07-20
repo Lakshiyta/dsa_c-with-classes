e5ee3253c35b1cf69395a9e76dde286d8e93b154
LAKSHIYTA BHATTI <121104382+Lakshiyta@users.noreply.github.com>
2024-07-20 14:02:13 +0530

    Add files via upload

---
+++ dsa_74.cpp
@@ -0,0 +1,17 @@
+//Accumalator with lambda
+//sum up of numbers with vector
+#include<iostream>
+#include<vector>
+#include<numeric>  //for std::accumaltor
+using namespace std;
+
+int main(){
+    vector<int> numbers={1,2,3,4,5};
+    //Using std::accumulate with a lambda to sum up the elements
+    int sum = accumulate(numbers.begin(), numbers.end(), 0, [](int total, int n){
+        return total + n;
+    });
+
+    cout<<"Sum: "<<sum<<endl; 
+    return 0;
+}
